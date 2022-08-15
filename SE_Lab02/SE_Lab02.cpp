3ee026f4cf9552e11d362b7f1df0fc05ec62f41e
DimaXDD <dimatruba2004@yandex.ru>
2022-08-15 18:17:53 +0300

    Add Lab2

---
+++ SE_Lab02/SE_Lab02.cpp
@@ -0,0 +1,26 @@
+﻿#include "stdafx.h"
+
+
+using namespace std;
+
+int sum(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int a, b;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "\nСумма чисел = " << sum(a, b) << endl;
+	cout << "Разность чисел = " << sub(a, b) << endl;
+	cout << "Приизведение чисел = " << mul(a, b) << endl;
+
+	system("pause");
+}
